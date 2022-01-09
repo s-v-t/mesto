b49e5c657f048a68b29e1e1e430b6069448134f8
Svetlana Tyrtyshnaya <svetlana.s.v.t@yandex.ru>
2022-01-09 14:07:53 +0300

    feat: замена картинок

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,12 @@
-const popupOpenButton = document.querySelector ('.profile__button-edit');
-const popupCloseButton = document.querySelector ('.popup__close');
-const popup = document.querySelector ('.popup');
+const popupOpenButton = document.querySelector('.profile__button-edit');
+const popupCloseButton = document.querySelector('.popup__close');
+const popup = document.querySelector('.popup');
 
 function togglePopup() {
     popup.classList.toggle('popup_opened');
 }
 
-function closePopupOnOverlayClick(event){
+function closePopupOnOverlayClick(event) {
     if (event.target === event.currentTarget) {
         popup.classList.remove('popup_opened');
     }
@@ -17,15 +17,15 @@ popupCloseButton.addEventListener('click', togglePopup);
 popup.addEventListener('click', closePopupOnOverlayClick);
 
 
-let formElement = document.querySelector ('.popup__form');
-let nameInput = formElement.querySelector ('.popup__form-name-input');
-let jobInput = formElement.querySelector ('.popup__form-job-input');
+let formElement = document.querySelector('.popup__form');
+let nameInput = formElement.querySelector('.popup__form-name-input');
+let jobInput = formElement.querySelector('.popup__form-job-input');
 
-function formSubmitHandler (evt) {
+function formSubmitHandler(evt) {
     evt.preventDefault();
 
-    let profile_name = document.querySelector ('.profile__info-name');
-    let profile_job = document.querySelector ('.profile__info-about');
+    let profile_name = document.querySelector('.profile__info-name');
+    let profile_job = document.querySelector('.profile__info-about');
 
     profile_name.textContent = nameInput.value;
     profile_job.textContent = jobInput.value;
