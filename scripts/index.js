1a48795f3cb228094ec26899ffece99a691a84ad
Svetlana Tyrtyshnaya <svetlana.s.v.t@yandex.ru>
2022-01-16 22:40:05 +0300

    hotfix/исправления ревью1

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,32 @@
 const popupOpenButton = document.querySelector('.profile__button-edit');
 const popupCloseButton = document.querySelector('.popup__close');
 const popup = document.querySelector('.popup');
-
-function togglePopup() {
-    popup.classList.toggle('popup_opened');
-}
-
-function closePopupOnOverlayClick(event) {
-    if (event.target === event.currentTarget) {
-        popup.classList.remove('popup_opened');
-    }
-}
-
-popupOpenButton.addEventListener('click', togglePopup);
-popupCloseButton.addEventListener('click', togglePopup);
-popup.addEventListener('click', closePopupOnOverlayClick);
-
-
 let formElement = document.querySelector('.popup__form');
 let nameInput = formElement.querySelector('.popup__form-name-input');
 let jobInput = formElement.querySelector('.popup__form-job-input');
+let profile_name = document.querySelector('.profile__info-name');
+let profile_job = document.querySelector('.profile__info-about');
+
+
+function OpenPopup() {
+    if (popup.classList.contains ('popup_opened') === false){
+        popup.classList.toggle('popup_opened');
+    }
+}
+
+function ClosePopup() {
+    if (popup.classList.contains ('popup_opened') === true){
+        popup.classList.toggle('popup_opened');
+    }
+}
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
 
-    let profile_name = document.querySelector('.profile__info-name');
-    let profile_job = document.querySelector('.profile__info-about');
-
     profile_name.textContent = nameInput.value;
     profile_job.textContent = jobInput.value;
+    ClosePopup();
 }
-
+popupOpenButton.addEventListener('click', OpenPopup);
+popupCloseButton.addEventListener('click', ClosePopup);
 formElement.addEventListener('submit', formSubmitHandler); 
